8ac752a10d36c9bf4bdf1407dd1ab46dc7287df7
deliarhmsri-rgb <deliarhmsri@gmail.com>
2025-11-13 10:10:20 +0700

    first-commit

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('./LuasLingkaran.php');
+
+use App\math\LuasLingkaran;
+
+$Lingkaran = new LuasLingkaran(5);
+// $Lingkaran->tampil('roda); //panggil method
+
+LuasLingkaran:;testing(); //panggil static method
